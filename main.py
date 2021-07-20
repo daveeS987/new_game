18f952c1f0efc643ac8ae738ce9c5008c71af213
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-07-20 12:51:15 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame, sys
+# initialize pygame
+pygame.init()
+
+# create a window
+# width and height
+win = pygame.display.set_mode((500, 500))
+# give window a name
+pygame.display.set_caption("First Game")
